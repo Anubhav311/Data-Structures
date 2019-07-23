e5f841e2d7a9dfc9709d71fa48b80337a3666ba6
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-24 00:52:30 +0530

    completed get_max in doubly_linked assignment

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -139,4 +139,14 @@ class DoublyLinkedList:
       self.remove_from_tail()
 
   def get_max(self):
-    pass
+    if not self.head:
+      return None
+    
+    max_value = self.head.value
+    current = self.head
+    while current:
+      if current.value > max_value:
+        max_value = current.value
+      current = current.next
+
+    return max_value
