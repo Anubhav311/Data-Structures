c2eb16e18bc2484eab81243c6ed613a8a046c480
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-25 04:05:04 +0530

    passed test for binary_search insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,11 +1,20 @@
-class BinarySearchTree:
+class BinarySearchTreeNode:
   def __init__(self, value):
     self.value = value
     self.left = None
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTreeNode(value)
+      else:
+        self.left.insert(value)
+    else:
+      if not self.right:
+        self.right = BinarySearchTreeNode(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
     pass
