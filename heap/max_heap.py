8d9608b8a6041767a62c1cedf0048ea185fcdfcb
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-25 19:27:30 +0530

    did some refactoring in MaxHeap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -2,9 +2,26 @@ class Heap:
   def __init__(self):
     self.storage = []
 
+  def get_max(self):
+    return self.storage[0]
+
+  def get_size(self):
+    return len(self.storage)
+
+  def get_parent_index(self, index):
+    return (index - 1) // 2
+
+  def get_left_child_index(self, index):
+    return index * 2 + 1
+
+  def get_right_child_index(self, index):
+    return index * 2 + 2
+
+
   def insert(self, value):
     self.storage.append(value)
     self._bubble_up(len(self.storage) - 1)
+
 
   def delete(self):
     topValue = self.storage[0]
@@ -15,15 +32,10 @@ class Heap:
     self._sift_down(0)
     return topValue
 
-  def get_max(self):
-    return self.storage[0]
-
-  def get_size(self):
-    return len(self.storage)
 
   def _bubble_up(self, index):
     while index > 0:
-      parent = (index - 1) // 2
+      parent = self.get_parent_index(index)
       if self.storage[index] > self.storage[parent]:
         self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
         index = parent
@@ -31,18 +43,19 @@ class Heap:
       else:
         break
 
-  def _sift_down(self, index):
-    largerChildrenIndex = (index * 2) + 1
 
-    if ((index * 2) + 2) < len(self.storage):
-      if self.storage[largerChildrenIndex] < self.storage[(index * 2) + 2]:
-        largerChildrenIndex = (index * 2) + 2
+  def _sift_down(self, index):
+    largerChildrenIndex = self.get_left_child_index(index)
+
+    if self.get_right_child_index(index) < len(self.storage):
+      if self.storage[largerChildrenIndex] < self.storage[self.get_right_child_index(index)]:
+        largerChildrenIndex = self.get_right_child_index(index)
       
       if self.storage[index] < self.storage[largerChildrenIndex]:
         self.storage[largerChildrenIndex], self.storage[index] = self.storage[index], self.storage[largerChildrenIndex]
         self._sift_down(largerChildrenIndex)
         
-    elif ((index * 2) + 1) < len(self.storage):
+    elif self.get_left_child_index(index) < len(self.storage):
       if self.storage[index] < self.storage[largerChildrenIndex]:
         self.storage[largerChildrenIndex], self.storage[index] = self.storage[index], self.storage[largerChildrenIndex]
         self._sift_down(largerChildrenIndex)
